2e1e741b65dae9adda9e583702d871aac399b899
Sandra Gonzales <sandra.gonzales@elastic.co>
2025-11-24 11:02:29 -0500

    create alert attachment

---
+++ x-pack/solutions/observability/plugins/observability_agent/server/attachments/register_attachments.ts
@@ -0,0 +1,21 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import type { AttachmentTypeDefinition } from '@kbn/onechat-server/attachments';
+import type { ObservabilityAgentPluginSetupDependencies } from '../types';
+import { createAlertAttachmentType } from './alert';
+
+export async function registerAttachments({
+  plugins,
+}: {
+  plugins: ObservabilityAgentPluginSetupDependencies;
+}) {
+  const attachmentTypes: AttachmentTypeDefinition<any, any>[] = [createAlertAttachmentType()];
+
+  for (const attachment of attachmentTypes) {
+    plugins.onechat.attachments.registerType(attachment);
+  }
+}
